4482b3ba387749b58a334846b9933bced7d4db6c
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-16 11:52:43 +0300

    ed

---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class home_view_body extends StatelessWidget {
+  const home_view_body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
