ffca95c12c3c464739e3332495da9b81570328b3
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-17 11:07:50 +0300

    style file

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -6,7 +6,7 @@ class home_view extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: home_view_body(),
     );
   }
