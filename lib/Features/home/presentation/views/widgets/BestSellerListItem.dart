6d7349d5fe593858c319c9c14332d9d69357a399
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-18 17:59:28 +0300

    rating add

--- lib/Features/home/presentation/views/widgets/BestSellerListItem.dart
+++ lib/Features/home/presentation/views/widgets/BestSellerListItem.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/ratingItem.dart';
 import 'package:bookly_app/core/utils/Assets.dart';
 import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
@@ -19,7 +20,7 @@ class BestSeller_item extends StatelessWidget {
             aspectRatio: 2.5 / 4,
             child: Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
+                borderRadius: BorderRadius.circular(12),
                 color: Colors.red,
                 image: const DecorationImage(
                   fit: BoxFit.fill,
@@ -31,62 +32,48 @@ class BestSeller_item extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                width: 200,
-                child: Text(
-                  "Harry Potter\nand the Goblet of Fire",
-                  overflow: TextOverflow.ellipsis,
-                  style: Style.titleMedium20,
-                  maxLines: 2,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 3.2,
                 ),
-              ),
-              const SizedBox(
-                height: 2,
-              ),
-              const Text(
-                "J.K. Rowling",
-                style: Style.titleMedium14,
-              ),
-              const SizedBox(
-                height: 2,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "19.99 €",
-                    style: Style.titleMedium20.copyWith(
-                      fontWeight: FontWeight.bold,
+                const SizedBox(
+                  width: 200,
+                  child: Text(
+                    "Harry Potter\nand the Goblet of Fire",
+                    overflow: TextOverflow.ellipsis,
+                    style: Style.titleMedium20,
+                    maxLines: 2,
+                  ),
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                const Text(
+                  "J.K. Rowling",
+                  style: Style.titleMedium14,
+                ),
+                const SizedBox(
+                  height: 2,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "19.99 €",
+                      style: Style.titleMedium20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 44.3,
-                  ),
-                  const Icon(
-                    FontAwesomeIcons.solidStar,
-                    color: Color(0xffFFDD4F),
-                  ),
-                  const SizedBox(
-                    width: 6.3,
-                  ),
-                  const Text(
-                    "4.8",
-                    style: Style.titleMedium16,
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    "(2390)",
-                    style: Style.titleMedium14.copyWith(
-                      color: Color(0xff707070),
+                    const SizedBox(
+                      width: 44.3,
                     ),
-                  )
-                ],
-              ),
-            ],
+                    RatingItem(),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
--- lib/Features/home/presentation/views/widgets/ratingItem.dart
+++ lib/Features/home/presentation/views/widgets/ratingItem.dart
@@ -1,10 +1,35 @@
-// import 'package:flutter/material.dart';
+import 'package:bookly_app/core/utils/style.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-// class RatingItem extends StatelessWidget {
-//   const RatingItem({super.key});
+class RatingItem extends StatelessWidget {
+  const RatingItem({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ;
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          color: Color(0xffFFDD4F),
+        ),
+        const SizedBox(
+          width: 6.3,
+        ),
+        const Text(
+          "4.8",
+          style: Style.titleMedium16,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          "(2390)",
+          style: Style.titleMedium14.copyWith(
+            color: Color(0xff707070),
+          ),
+        ),
+      ],
+    );
+  }
+}
