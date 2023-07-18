118ed41fe331f478701951cff5a0e0daf5c18666
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-18 16:38:21 +0300

    edit

--- lib/Features/home/presentation/views/widgets/BestSellerListItem.dart
+++ lib/Features/home/presentation/views/widgets/BestSellerListItem.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/Assets.dart';
 import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSeller_item extends StatelessWidget {
   const BestSeller_item({super.key});
@@ -8,7 +9,7 @@ class BestSeller_item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 125,
+      height: 110,
       child: Row(
         children: [
           const SizedBox(
@@ -30,9 +31,10 @@ class BestSeller_item extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          const Column(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              const SizedBox(
                 width: 200,
                 child: Text(
                   "Harry Potter\nand the Goblet of Fire",
@@ -40,6 +42,49 @@ class BestSeller_item extends StatelessWidget {
                   style: Style.titleMedium20,
                   maxLines: 2,
                 ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              const Text(
+                "J.K. Rowling",
+                style: Style.titleMedium14,
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "19.99 €",
+                    style: Style.titleMedium20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 44.3,
+                  ),
+                  const Icon(
+                    FontAwesomeIcons.solidStar,
+                    color: Color(0xffFFDD4F),
+                  ),
+                  const SizedBox(
+                    width: 6.3,
+                  ),
+                  const Text(
+                    "4.8",
+                    style: Style.titleMedium16,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "(2390)",
+                    style: Style.titleMedium14.copyWith(
+                      color: Color(0xff707070),
+                    ),
+                  )
+                ],
               ),
             ],
           )
---
+++ lib/Features/home/presentation/views/widgets/ratingItem.dart
@@ -0,0 +1,10 @@
+// import 'package:flutter/material.dart';
+
+// class RatingItem extends StatelessWidget {
+//   const RatingItem({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ;
+//   }
+// }
