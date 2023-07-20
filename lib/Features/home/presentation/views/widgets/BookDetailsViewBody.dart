562c13293c5c22d588ceb4435c47061f39e6f022
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-20 10:45:29 +0300

    CustomAppbar BookDetails

--- lib/Features/home/presentation/views/widgets/BookDetailsViewBody.dart
+++ lib/Features/home/presentation/views/widgets/BookDetailsViewBody.dart
@@ -5,6 +5,29 @@ class Book_details_View_Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Column(
+      children: [],
+    );
+  }
+}
+
+class CustomBookDetailsAppbar extends StatelessWidget {
+  const CustomBookDetailsAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.close),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.shopping_cart_outlined),
+        ),
+      ],
+    );
   }
 }
