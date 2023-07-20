cc0f788ae6036a744761022a9598f5f4720961b1
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-20 14:22:45 +0300

    Create files

---
+++ lib/core/widgets/Custom_Button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Custom_button extends StatelessWidget {
+  const Custom_button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
