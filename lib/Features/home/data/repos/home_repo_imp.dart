14f67c25fe414cf5084add1d5ec35ac39ca11bb7
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-22 12:03:09 +0300

    code imp home repo

--- lib/Features/home/data/repos/home_repo_imp.dart
+++ lib/Features/home/data/repos/home_repo_imp.dart
@@ -1,13 +1,27 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utils/Api_Services.dart';
 import 'package:dartz/dartz.dart';
 
 class Home_repo_imp extends HomeRepo {
+  final Api_Services api_service;
+
+  Home_repo_imp(this.api_service);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await api_service.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&Sorting=newest &q=subject : programming");
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
--- lib/core/utils/Api_Services.dart
+++ lib/core/utils/Api_Services.dart
@@ -2,11 +2,11 @@ import 'package:dio/dio.dart';
 
 class Api_Services {
   final Dio _dio;
-  final String base_Url = "https://www.googleapis.com/books/v1";
+  final String _base_Url = "https://www.googleapis.com/books/v1";
   Api_Services(this._dio);
 
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await _dio.get("$base_Url$endPoint");
+    var response = await _dio.get("$_base_Url$endPoint");
     return response.data;
   }
 }
