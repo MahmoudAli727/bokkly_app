d9deac9900493857e47b0127a2f39e3877ddcc28
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-22 11:54:34 +0300

    api code

--- lib/core/utils/Api_Services.dart
+++ lib/core/utils/Api_Services.dart
@@ -1,1 +1,12 @@
-class Api_Services {}
+import 'package:dio/dio.dart';
+
+class Api_Services {
+  final Dio _dio;
+  final String base_Url = "https://www.googleapis.com/books/v1";
+  Api_Services(this._dio);
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await _dio.get("$base_Url$endPoint");
+    return response.data;
+  }
+}
