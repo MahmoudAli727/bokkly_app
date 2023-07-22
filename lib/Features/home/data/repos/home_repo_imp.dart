18465c00a85bd710a1cc961fbc363e0291f03bd9
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-22 11:34:21 +0300

    Create home repo imp file

---
+++ lib/Features/home/data/repos/home_repo_imp.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class Home_repo_imp extends HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+}
