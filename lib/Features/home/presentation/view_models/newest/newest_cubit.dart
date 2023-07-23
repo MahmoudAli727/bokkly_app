a37cd3cdc10f61cc723a22dc102f01f68e3b610e
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-23 23:06:22 +0300

    cubit done

--- lib/Features/home/presentation/view_models/newest/newest_cubit.dart
+++ lib/Features/home/presentation/view_models/newest/newest_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_state.dart';
 
 class NewestCubit extends Cubit<NewestState> {
-  NewestCubit() : super(NewestInitial());
+  NewestCubit(this.home_repo) : super(NewestInitial());
+  final HomeRepo home_repo;
+  Future<void> fetchNewestBooks() async {
+    emit(Newsetloading());
+    var result = await home_repo.fetchFeatureBooks();
+    result.fold((Failure) {
+      emit(NewsetFailure(Failure.toString()));
+    }, (books) {
+      emit(NewsetSuccess(books));
+    });
+  }
 }
