76bad12a7b739b440a91e1905b528ccac071e7a3
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-23 21:20:35 +0300

    as

--- lib/Features/home/data/repos/home_repo_imp.dart
+++ lib/Features/home/data/repos/home_repo_imp.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/Api_Services.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class Home_repo_imp extends HomeRepo {
   final Api_Services api_service;
@@ -20,7 +21,10 @@ class Home_repo_imp extends HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
