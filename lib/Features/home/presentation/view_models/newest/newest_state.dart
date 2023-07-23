a756456b4449e4dbba11e83de322fc1bd244d920
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-23 23:03:08 +0300

    done

--- lib/Features/home/presentation/view_models/newest/newest_state.dart
+++ lib/Features/home/presentation/view_models/newest/newest_state.dart
@@ -8,3 +8,17 @@ abstract class NewestState extends Equatable {
 }
 
 class NewestInitial extends NewestState {}
+
+class Newsetloading extends NewestState {}
+
+class NewsetSuccess extends NewestState {
+  final List<BookModel> books;
+
+  const NewsetSuccess(this.books);
+}
+
+class NewsetFailure extends NewestState {
+  final String errMessage;
+
+  const NewsetFailure(this.errMessage);
+}
