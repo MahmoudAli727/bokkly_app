ea59035d9d47f799d4a66ce0f8de320767a6a994
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-23 21:23:22 +0300

    add function

--- lib/Features/home/data/repos/home_repo_imp.dart
+++ lib/Features/home/data/repos/home_repo_imp.dart
@@ -29,8 +29,20 @@ class Home_repo_imp extends HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await api_service.get(
+          endPoint: "volumes?Filtering=free-ebooks&q=subject : programming");
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
