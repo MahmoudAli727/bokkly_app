a331322daa18b6ea97454e871451d760dda8879f
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-24 15:25:05 +0300

    bloc builder

--- lib/Features/home/presentation/views/widgets/featurelist.dart
+++ lib/Features/home/presentation/views/widgets/featurelist.dart
@@ -1,25 +1,39 @@
+import 'package:bookly_app/Features/home/presentation/view_models/Feature_books_cubit/feature_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/CustomListitem.dart';
+import 'package:bookly_app/core/utils/CustomCircular.dart';
+import 'package:bookly_app/core/widgets/Custom_error.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeatureList extends StatelessWidget {
   const FeatureList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListItem(),
-            );
-          },
-        ),
-      ),
+    return BlocBuilder<FeatureCubit, FeatureState>(
+      builder: (context, state) {
+        if (state is FeatureSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomListItem(),
+                  );
+                },
+              ),
+            ),
+          );
+        } else if (state is FeatureFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
