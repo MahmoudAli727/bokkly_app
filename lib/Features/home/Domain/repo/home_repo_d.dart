47fd28cdc4b3387d9d51c756523e312e0237fa89
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-29 11:18:08 +0300

    creae home repo entity

---
+++ lib/Features/home/Domain/repo/home_repo_d.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/Features/home/Domain/entities/Book_entity.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class home_repo_d {
+  Future<Either<Failure, List<book_entity>>> fetchFeauredBookentity();
+  Future<Either<Failure, List<book_entity>>> fetchNewesBookentity();
+}
