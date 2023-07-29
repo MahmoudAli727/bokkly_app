58d9c81b0e5b1a74da153afead18dc4c0e1beb25
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-29 11:14:20 +0300

    entity book

---
+++ lib/Features/home/Domain/entities/Book_entity.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, camel_case_types
+class book_entity {
+  final String title;
+  final String authorname;
+  final String image;
+  final num price;
+  final num rating;
+  book_entity({
+    required this.title,
+    required this.authorname,
+    required this.image,
+    required this.price,
+    required this.rating,
+  });
+}
