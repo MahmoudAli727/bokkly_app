f663952d88830484134f4bf425860afc272d7af1
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-30 07:52:15 +0300

    create homedatasourceremote

--- lib/Features/home/Domain/entities/Book_entity.dart
+++ lib/Features/home/Domain/entities/Book_entity.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, camel_case_types
 class book_entity {
+  final String bookId;
   final String title;
-  final String authorname;
-  final String image;
-  final num price;
-  final num rating;
+  final String? authorname;
+  final String? image;
+  final num? price;
+  final num? rating;
   book_entity({
+    required this.bookId,
     required this.title,
     required this.authorname,
     required this.image,
