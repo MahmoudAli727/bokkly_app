6d3475a31e4c2490a2c33d94c420fbb4ccddff66
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-30 12:14:52 +0300

    Hive main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/home/Domain/entities/Book_entity.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo_imp.dart';
 import 'package:bookly_app/Features/home/presentation/view_models/Feature_books_cubit/feature_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/view_models/newest/newest_cubit.dart';
@@ -7,9 +8,12 @@ import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
   setup();
+  Hive.registerAdapter(bookentityAdapter());
+  await Hive.openBox(kFeaturedBox);
   runApp(const BokklyApp());
 }
 
