725a75ff2916e12c58814c8804096de38d1ebe47
mahmoud ali <mahmoudalio353@gmail.com>
2023-07-30 11:40:34 +0300

    create file to function

--- lib/Features/home/data/data_sources/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -1,20 +1,28 @@
+import 'package:bookly_app/Features/home/Domain/entities/Book_entity.dart';
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/Api_Services.dart';
 
 abstract class HomeRemoteDataSource {
-  Future<List<BookModel>> fetchBestSellerBooks();
-  Future<List<BookModel>> fetchFeatureBooks();
+  Future<List<book_entity>> fetchFeatureBooks();
+  Future<List<book_entity>> fetchBestSellerBooks();
 }
 
 class HomeRemoteDataSourceImp extends HomeRemoteDataSource {
+  final Api_Services api_services;
+
+  HomeRemoteDataSourceImp(this.api_services);
+
   @override
-  Future<List<BookModel>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<List<book_entity>> fetchFeatureBooks() async {
+    var data = await api_services.get(endPoint: "");
+    List<book_entity> books = getBookslist(data);
+    return books;
   }
 
   @override
-  Future<List<BookModel>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<List<book_entity>> fetchBestSellerBooks() async {
+    var data = await api_services.get(endPoint: "");
+    List<book_entity> books = getBookslist(data);
+    return books;
   }
 }
