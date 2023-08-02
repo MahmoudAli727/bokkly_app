81852eea2d1157272067e25d840704c9bad6a469
mahmoud ali <mahmoudalio353@gmail.com>
2023-08-02 08:19:28 +0300

    Create loading indicator list

---
+++ lib/Features/home/presentation/views/widgets/Custom_book_image_loading_indicator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomBookImageLoadingIndicator extends StatelessWidget {
+  const CustomBookImageLoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.6 / 4,
+      child: ClipRRect(),
+    );
+  }
+}
